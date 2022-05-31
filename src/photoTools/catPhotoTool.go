91e8e95b7b460191e2b51e0e91db124584750c2b
JasonSatherr <jsather@ucsd.edu>
2022-05-31 10:14:31 -0700

    comment code

--- src/photoTools/catPhotoTool.go
+++ src/photoTools/catPhotoTool.go
@@ -13,21 +13,25 @@ type CatPhotoTool struct {
 
 func (cpt CatPhotoTool) GenerateImage() (image.Image, error) {
 	fmt.Printf("\n getting an image \n")
-	existingImageFile, err := os.Open("./pics/cat.jpg")
+	existingImageFile, err := os.Open("./pics/cat.jpg") //get the cat pic
+
 	if err != nil {
-		// Handle error
+		//Bad because we do not unpack the err (*PathError)
+		//which contains more information
 		return nil, errors.New("COULD NOT FIND IMAGE")
 	}
 
 	defer existingImageFile.Close()
 
 	// Calling the generic image.Decode() will tell give us the data
-	// and type of image it is as a string. We expect "png"
+	// and type of image it is as a string. We expect "jpg"
 	imageData, imageType, err := image.Decode(existingImageFile)
+
 	if err != nil {
-		// Handle error
+		// most likely more we can decode... maybe just pass err back instead of error?
 		return nil, errors.New("COULD NOT DECODE IMAGE")
 	}
+
 	fmt.Println(imageData)
 	fmt.Println(imageType)
 	return imageData, nil
