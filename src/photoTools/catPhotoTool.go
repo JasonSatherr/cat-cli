2a4cd1af5ce3c03591af700ba676cbe843c6b05d
JasonSatherr <jsather@ucsd.edu>
2022-06-01 15:16:20 -0700

    grabbing cat pic from a https url

--- src/photoTools/catPhotoTool.go
+++ src/photoTools/catPhotoTool.go
@@ -3,28 +3,30 @@ package photoTools
 import (
 	"fmt"
 	"image"
-	"os"
+	"net/http"
 )
 
 type CatPhotoTool struct {
 	//sound string
 }
 
+//just move some of this logic out to something that all the classes of the interface can share?
 func (cpt CatPhotoTool) GenerateImage() (image.Image, error) {
 	fmt.Printf("\n getting an image \n")
-	existingImageFile, err := os.Open("./pics/cat.jpg") //get the cat pic
-
+	//existingImageFile, err := os.Open("./pics/cat.jpg") //get the cat pic
+	existingImageFile, err := http.Get("https://cdn2.thecatapi.com/images/9fm.jpg")
+	//https://cdn2.thecatapi.com/images/9fm.jpg
 	if err != nil {
 		//Bad because we do not unpack the err (*PathError)
 		//which contains more information
 		return nil, err
 	}
 
-	defer existingImageFile.Close()
+	defer existingImageFile.Body.Close()
 
 	// Calling the generic image.Decode() will tell give us the data
 	// and type of image it is as a string. We expect "jpg"
-	imageData, _, err := image.Decode(existingImageFile)
+	imageData, _, err := image.Decode(existingImageFile.Body)
 
 	if err != nil {
 		// most likely more we can decode... maybe just pass err back instead of error?
